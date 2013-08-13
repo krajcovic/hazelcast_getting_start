f52dff54c3eac5d166665c2402b747b6a5bb7107
Krajcovic <dusan.krajcovic@gmail.com>
2013-08-13 14:21:46 +0200

    base skelet.

---
+++ src/main/java/cz/krajcovic/hazelcast/GettingStarted/App.java
@@ -0,0 +1,13 @@
+package cz.krajcovic.hazelcast.GettingStarted;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
