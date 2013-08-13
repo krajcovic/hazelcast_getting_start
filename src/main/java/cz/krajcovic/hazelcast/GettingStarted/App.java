b7e6f4bddde403db0fe1892b22d357f0f68b2c69
Krajcovic <dusan.krajcovic@gmail.com>
2013-08-13 14:34:09 +0200

    First hazelcast application

--- src/main/java/cz/krajcovic/hazelcast/GettingStarted/App.java
+++ src/main/java/cz/krajcovic/hazelcast/GettingStarted/App.java
@@ -1,5 +1,11 @@
 package cz.krajcovic.hazelcast.GettingStarted;
 
+import java.util.Queue;
+
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,22 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        HazelcastInstance instance = Hazelcast.newHazelcastInstance();
+        IMap<Integer, String> mapCustomers = instance.getMap("customers");
+        mapCustomers.put(1, "Joe");
+        mapCustomers.put(2, "Ali");
+        mapCustomers.put(3, "Avi");
+        
+        System.out.println("Cusomer with key 1: " + mapCustomers.get(1));
+        System.out.println("Map size:" + mapCustomers.size());
+        
+        Queue<String> queueCustomers = instance.getQueue("customers");
+        queueCustomers.offer("Tom");
+    	queueCustomers.offer("Mary");
+    	queueCustomers.offer("Jane");
+        System.out.println("First customer: " + queueCustomers.poll());
+        System.out.println("Second customer: " + queueCustomers.peek());
+        System.out.println("Queue size: " + queueCustomers.size());
+        
     }
 }
